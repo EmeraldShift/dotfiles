583d50f405b040506db511b4e5bbe543db2377b3
Michael Jarrett <mcjarrett0@gmail.com>
2020-02-23 15:32:17 -0600

    more fun vim stuff + tmux

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
 
-# Bash
-rm -f ~/.bashrc
-ln -s ~/.dotfiles/bash/bashrc ~/.bashrc
-
-# Nano
-rm -f ~/.nanorc
-ln -s ~/.dotfiles/nano/nanorc ~/.nanorc
-
-# Vim
-rm -f ~/.vimrc
-ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s -f ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -s -f ~/.dotfiles/nano/nanorc ~/.nanorc
+ln -s -f ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
+source ~/.bashrc
 
 echo 'Dotfiles installed.'
