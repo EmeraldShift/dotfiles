de489b0f0c97dd418c4fb00900fb7becc0fe2e59
Michael Jarrett <mcjarrett0@gmail.com>
2020-02-23 16:15:01 -0600

    Add bash_profile, fix recursive vim mappings

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 
 ln -s -f ~/.dotfiles/bash/bashrc ~/.bashrc
+ln -s -f ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s -f ~/.dotfiles/nano/nanorc ~/.nanorc
 ln -s -f ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
-source ~/.bashrc
+source ~/.bash_profile
 
 echo 'Dotfiles installed.'
