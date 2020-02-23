28d7054af87c99e9a3317eda7aa575d83d32e618
Michael Jarrett <mcjarrett0@gmail.com>
2020-02-23 17:41:59 -0600

    Fix tmux files

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ ln -s -f ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -s -f ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s -f ~/.dotfiles/nano/nanorc ~/.nanorc
 ln -s -f ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s -f ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
 source ~/.bash_profile
 
