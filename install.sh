309db8db8c1fe8770a1d939f11ab95920b5e1a9a
Michael Jarrett <mcj@cs.utexas.edu>
2020-02-24 13:26:06 -0600

    Vim plugins!

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 
 ln -s -f ~/.dotfiles/bash/bashrc ~/.bashrc
+source ~/.bash_profile
+
 ln -s -f ~/.dotfiles/bash/bash_profile ~/.bash_profile
 ln -s -f ~/.dotfiles/nano/nanorc ~/.nanorc
 ln -s -f ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s -f ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
 ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
-source ~/.bash_profile
+ln -s -f ~/.dotfiles/vim ~/.vim
+vim +PlugInstall
 
 echo 'Dotfiles installed.'
