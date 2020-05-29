6b81e65efa204c0ff581a8cb40b62178b02ed3a3
Michael Jarrett <michaelj@stripe.com>
2020-05-29 02:55:59 -0500

    Improve install script

--- install.sh
+++ install.sh
@@ -1,11 +1,20 @@
+set -euo pipefail
 
 ln -s -f ~/.dotfiles/bash/bashrc ~/.bashrc
-source ~/.bash_profile
+. ~/.bash_profile
 
-ln -s -f ~/.dotfiles/bash/bash_profile ~/.bash_profile
+### Don't override profile, it usually has important stuff in it.
+### Instead, manually add the call to .bashrc at the end of it.
+### ln -s -f ~/.dotfiles/bash/bash_profile ~/.bash_profile
+
+# nano
 ln -s -f ~/.dotfiles/nano/nanorc ~/.nanorc
+
+# tmux
 ln -s -f ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 ln -s -f ~/.dotfiles/tmux/tmux.conf.local ~/.tmux.conf.local
+
+# vim
 ln -s -f ~/.dotfiles/vim/vimrc ~/.vimrc
 ln -s -f ~/.dotfiles/vim ~/.vim
 vim +PlugInstall +qall
